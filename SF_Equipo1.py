df6a1de12033c827bce05ab8fe24da3a16eb51b5
Charlyval01 <88901402+Charlyval01@users.noreply.github.com>
2023-11-25 14:07:22 -0600

    SF_Equipo1.py

--- SF_Equipo1.py
+++ SF_Equipo1.py
@@ -14,9 +14,9 @@ st.set_page_config(layout="wide", page_title= "SF Incident Analytics", page_icon
 # LOAD DATA ONCE
 @st.cache_resource
 def load_data():
-    path = "Police_Department_Incident_Reports__2018_to_Present.csv"
+    path = "Police_Department_Incident_Reports__2018_to_Present.zip"
     if not os.path.isfile(path):
-        path = f"/Users/mivalenciap/Desktop/{path}"
+        path = f"https://raw.githubusercontent.com/Charlyval01/San-Francisco-Insights-streamlit-/main/{path}"
 
     data = pd.read_csv(
         path,
